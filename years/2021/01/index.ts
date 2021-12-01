8931bb9d5485a3d689831f1a4e09b4e464fc338b
Mike D <michaelduller@hotmail.com>
2021-12-01 10:04:28 +0000

    day 1

--- years/2021/01/index.ts
+++ years/2021/01/index.ts
@@ -4,6 +4,7 @@ import * as test from "../../../util/test";
 import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
+import { exit } from "process";
 
 const YEAR = 2021;
 const DAY = 1;
@@ -13,18 +14,38 @@ const DAY = 1;
 // problem url  : https://adventofcode.com/2021/day/1
 
 async function p2021day1_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const data = input.split("\n").map(Number);
+	let count = 0;
+	for (let i = 1; i <= data.length; i++) {
+		if (data[i] > data[i-1]) {
+			count += 1;
+		}
+	}
+	return count;
 }
 
 async function p2021day1_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const data = input.split("\n").map(Number);
+	let slidyBoi = undefined
+	let count = 0;
+
+	for (let i =0; i < data.length-2; i++) {
+		const slidy = data[i] + data[i+1] + data[i+2]
+		if(slidyBoi !== undefined && slidy > slidyBoi) {
+			count += 1;
+		}
+		slidyBoi = slidy;
+	}
+
+	return count;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{"input": "199\n200\n208\n210\n200\n207\n240\n269\n260\n263", "expected": "7"}];
 	const part2tests: TestCase[] = [];
 
 	// Run tests
+	if(true) {
 	test.beginTests();
 	await test.section(async () => {
 		for (const testCase of part1tests) {
@@ -37,6 +58,7 @@ async function run() {
 		}
 	});
 	test.endTests();
+}
 
 	// Get input and run program while measuring performance
 	const input = await util.getInput(DAY, YEAR);
