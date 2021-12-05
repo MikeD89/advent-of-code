b6592491e8374455fcc969071b958943adcc9b09
Mike D <michaelduller@hotmail.com>
2021-12-05 12:47:32 +0000

    day 5

--- years/2021/05/index.ts
+++ years/2021/05/index.ts
@@ -4,6 +4,7 @@ import * as test from "../../../util/test";
 import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
+import { exit } from "process";
 
 const YEAR = 2021;
 const DAY = 5;
@@ -12,41 +13,76 @@ const DAY = 5;
 // data path    : D:\Work\advent-of-code\years\2021\05\data.txt
 // problem url  : https://adventofcode.com/2021/day/5
 
-async function p2021day5_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+function getLine(l: string): number[][] {
+	const p = l.split(" -> ");
+	const f = p[0].split(",");
+	const t = p[1].split(",");
+
+	const fN = [Number(f[0]), Number(f[1])]
+	const tN = [Number(t[0]), Number(t[1])]
+
+	return [fN, tN];
 }
 
-async function p2021day5_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+function isHorizontal(line: number[][]) {
+	return line[0][0] === line[1][0] || line[0][1] === line[1][1];
+}
+
+function getPoints(line: number[][]) {
+	const p = [];
+
+	const startX = line[0][0]; 
+	const startY = line[0][1]; 
+
+	const lX = line[1][0] - startX;
+	const lY = line[1][1] - startY;
+	const m = lX === 0 ? 0 : (lX > 0 ? 1 : -1);
+	const n = lY === 0 ? 0 : (lY > 0 ? 1 : -1);
+
+	for(let i = 0; i <= Math.max(Math.abs(lX), Math.abs(lY)); i++){
+		p.push((startX + (i * m)) + "," + (startY + (i * n)));
+	}
+
+	return p;
+}
+
+function countPoints(points: string[]) {
+	const count = new Map<string, number>();
+	points.forEach(p => {
+		const v = count.get(p) ?? 0;
+		count.set(p, v + 1)
+	})
+	return count;
+}
+
+function getWithAtLeast(count: Map<string, number>, n: number) {
+	let i = 0;
+	count.forEach((value, key) => {if(value>=n) i+=1})
+	return i;
+}
+
+async function p2021day5(input: number[][][], h: boolean) {
+	const f = h ? input.filter(isHorizontal) : input;
+	const p = f.map(getPoints).flat();
+	const c = countPoints(p);
+	const v =  getWithAtLeast(c, 2);
+	return v;
 }
 
 async function run() {
 	const part1tests: TestCase[] = [];
 	const part2tests: TestCase[] = [];
 
-	// Run tests
-	test.beginTests();
-	await test.section(async () => {
-		for (const testCase of part1tests) {
-			test.logTestResult(testCase, String(await p2021day5_part1(testCase.input, ...(testCase.extraArgs || []))));
-		}
-	});
-	await test.section(async () => {
-		for (const testCase of part2tests) {
-			test.logTestResult(testCase, String(await p2021day5_part2(testCase.input, ...(testCase.extraArgs || []))));
-		}
-	});
-	test.endTests();
-
 	// Get input and run program while measuring performance
-	const input = await util.getInput(DAY, YEAR);
+	const input = await (await util.getInput(DAY, YEAR)).split("\n");
+	const d = input.map(getLine);
 
 	const part1Before = performance.now();
-	const part1Solution = String(await p2021day5_part1(input));
+	const part1Solution = String(await p2021day5(d, true));
 	const part1After = performance.now();
 
 	const part2Before = performance.now()
-	const part2Solution = String(await p2021day5_part2(input));
+	const part2Solution = String(await p2021day5(d, false));
 	const part2After = performance.now();
 
 	logSolution(5, 2021, part1Solution, part2Solution);
